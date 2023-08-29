cf76dce96e90fde2506dad952040f4a21e05156b
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-08-29 18:07:48 +0300

    initial test setup

--- Tests/iOS Team Weekly 2023-08-29Tests/CvvPinServiceTests.swift
+++ Tests/iOS Team Weekly 2023-08-29Tests/CvvPinServiceTests.swift
@@ -46,7 +46,7 @@ final class CvvPinService {
     func exchangeKey(completion: @escaping ExchangeKeyCompletion) {
         
         getProcessingSessionCodeService.get { [unowned self] result in
-         
+            
             switch result {
             case let .failure(error):
                 completion(.failure(error))
@@ -75,6 +75,85 @@ final class CvvPinServiceTests: XCTestCase {
     
     func test() {
         
-        // XCTFail()
+        let (sut, getProcessingSessionCodeServiceSpy, exchangeKeyServiceSpy, confirmExchangeServiceSpy) = makeSUT()
+        
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: CvvPinService,
+        getProcessingSessionCodeServiceSpy: GetProcessingSessionCodeServiceSpy,
+        exchangeKeyServiceSpy: ExchangeKeyServiceSpy,
+        confirmExchangeServiceSpy: ConfirmExchangeServiceSpy
+    ) {
+        
+        let getProcessingSessionCodeServiceSpy = GetProcessingSessionCodeServiceSpy()
+        let exchangeKeyServiceSpy = ExchangeKeyServiceSpy()
+        let confirmExchangeServiceSpy = ConfirmExchangeServiceSpy()
+        
+        let sut = CvvPinService(
+            getProcessingSessionCodeService: getProcessingSessionCodeServiceSpy,
+            exchangeKeyService: exchangeKeyServiceSpy,
+            confirmExchangeService: confirmExchangeServiceSpy
+        )
+        
+        // track memory leaks
+        
+        return (sut, getProcessingSessionCodeServiceSpy, exchangeKeyServiceSpy, confirmExchangeServiceSpy)
+    }
+    
+    private final class GetProcessingSessionCodeServiceSpy: GetProcessingSessionCodeService {
+        
+        private var completions = [(Result<ProcessingSessionCode, Error>) -> Void]()
+        
+        func get(completion: @escaping (Result<ProcessingSessionCode, Error>) -> Void) {
+            
+            completions.append(completion)
+        }
+        
+        func complete(
+            with result: Result<ProcessingSessionCode, Error>,
+            at index: Int = 0
+        ) {
+            completions[index](result)
+        }
+    }
+    
+    private final class ExchangeKeyServiceSpy: ExchangeKeyService {
+        
+        private var messages = [(code: ProcessingSessionCode, completion: (Result<KeyExchange, Error>) -> Void)]()
+        
+        var codes: [ProcessingSessionCode] { messages.map(\.code) }
+        
+        func exchange(_ code: ProcessingSessionCode, completion: @escaping (Result<KeyExchange, Error>) -> Void) {
+            
+            messages.append((code, completion))
+        }
+        
+        func complete(
+            with result: Result<KeyExchange, Error>,
+            at index: Int = 0
+        ) {
+            messages[index].completion(result)
+        }
+    }
+    
+    private final class ConfirmExchangeServiceSpy: ConfirmExchangeService {
+        
+//        private let stub: Result<Void, Error>
+//
+//        init(stub: Result<Void, Error>) {
+//
+//            self.stub = stub
+//        }
+//
+//        func confirm(completion: @escaping (Result<Void, Error>) -> Void) {
+//
+//            completion(stub)
+//        }
     }
 }
