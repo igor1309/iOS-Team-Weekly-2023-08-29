1511cdc23a16995b890beb6b2a6c97851ee206fd
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-08-29 17:31:33 +0300

    initial cleanup

--- Sources/iOS Team Weekly 2023-08-29/iOS_Team_Weekly_2023_08_29.swift
+++ Sources/iOS Team Weekly 2023-08-29/iOS_Team_Weekly_2023_08_29.swift
@@ -1,6 +1,1 @@
-public struct iOS_Team_Weekly_2023_08_29 {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
-}
---
+++ Tests/iOS Team Weekly 2023-08-29Tests/CvvPinServiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  CvvPinServiceTests.swift
+//  
+//
+//  Created by Igor Malyarov on 29.08.2023.
+//
+
+import XCTest
+
+final class CvvPinServiceTests: XCTestCase {
+    
+    func test() {
+        
+        // XCTFail()
+    }
+}
